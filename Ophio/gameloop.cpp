514e22f8f0c187feecb6f4f7ea4bfe97682d8c36
Paul Bagnis <paul.bagnis@gmail.com>
2021-03-15 15:48:17 +0100

    First text hud

--- Ophio/gameloop.cpp
+++ Ophio/gameloop.cpp
@@ -11,6 +11,29 @@ GameLoop::GameLoop(QWidget *parent)
     fightmap->tile_px=16;
     fightmap->addScene();
 
+    // HUD
+
+    // Start Button
+    startButton->setStyleSheet("color: black; background-color: green;");
+    startButton->setText("Start");
+    startButton->setFixedSize(80,80);
+    startButton->move((scene->width()-67), (scene->height()-80));
+    scene->addWidget(startButton);
+
+    // Action Button
+    actionButton->setStyleSheet("color: black; background-color: white;");
+    actionButton->setText("actionButton");
+    actionButton->setFixedSize(300,40);
+    actionButton->move(5, (scene->height()-80));
+    scene->addWidget(actionButton);
+
+    // Move Button
+    moveButton->setStyleSheet("color: black; background-color: white;");
+    moveButton->setText("moveButton");
+    moveButton->setFixedSize(300,40);
+    moveButton->move(5, (scene->height()-40));
+    scene->addWidget(moveButton);
+
     /* Vie et Barre de vie */
     Ophio *ophio = new Ophio();
     /* Test pour couleurs HpBar */
--- Ophio/gameloop.h
+++ Ophio/gameloop.h
@@ -3,6 +3,7 @@
 #include <QGraphicsView>
 #include <QWidget>
 #include <QGraphicsScene>
+#include <QPushButton>
 #include "tile.h"
 #include "ophio.h"
 #include "entity.h"
@@ -14,6 +15,10 @@ public:
     GameLoop(QWidget * parent=0);
 
     QGraphicsScene* scene = new QGraphicsScene();
+    QPushButton* startButton = new QPushButton();
+    QPushButton* actionButton = new QPushButton();
+    QPushButton* moveButton = new QPushButton();
+
 };
 
 #endif // GAMELOOP_H
