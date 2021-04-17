5c1b9b61a592df6a11dfffc9abcc5cda051ed53f
francoisg <francois.guern@trialog.com>
2021-04-17 23:16:02 +0200

    set map size width and heigh / set and get entities on top

--- Ophio/fightmap.cpp
+++ Ophio/fightmap.cpp
@@ -1,17 +1,13 @@
 #include "fightmap.h"
-#include "tile.h"
-#include <QDebug>
 
-FightMap::FightMap(int scene_size, int tile_size, TilesetSplitter *tileSplit)
+FightMap::FightMap(int sceneWidth, int sceneHeigh, TilesetSplitter *tileSplit):
+    sceneWidth_(sceneWidth),
+    sceneHeigh_(sceneHeigh)
 {
-    scene_size_=scene_size;
-    tile_size_=tile_size;
-
-    for (int i=0; i<scene_size_; i++) {
+    for (int i=0; i<sceneHeigh_; i++) {
         map_.append(QList<Tile*>());
-        for (int j=0; j<scene_size_; j++) {
-            Tile* tile = new Tile(tile_size, tileSplit);
-            map_[i].append(tile);
+        for (int j=0; j<sceneWidth_; j++) {
+            map_[i].append(new Tile(tileSplit));
         }
     }
 }
@@ -27,18 +23,17 @@ QList<QGraphicsPixmapItem *> FightMap::getFightMap()
 QList<QGraphicsPixmapItem*> FightMap::getFloor()
 {
     QList<QGraphicsPixmapItem*> to_render;
-    int tile_pace = tile_px * 0.75;
-    int x_offset = tile_pace * 0.75 * tile_size_ + tile_pace * tile_size_ * ( scene_size_ - 1 ) ;
-    int y_offset = 12 * tile_size_;
-    for (int i=0; i<scene_size_; i++) {
-        for (int j=0; j<scene_size_; j++) {
-            map_[i][j]->topSprite_->setPos(x_offset*2, y_offset);
+    int posX = 0;
+    int posY = 0;
+    for (int i=0; i< sceneHeigh_; i++) {
+        for (int j=0; j<sceneWidth_; j++) {
+            map_[i][j]->topSprite_->setPos(posX, posY);
             to_render.append(map_[i][j]->topSprite_);
-            x_offset += tile_pace * tile_size_;
-            y_offset += tile_pace * tile_size_;
+            posX += tileSize_*2;
+            posY += tileSize_;
         }
-        x_offset -= tile_pace * tile_size_ * ( scene_size_ + 1 );
-        y_offset -= tile_pace * tile_size_ * ( scene_size_ - 1 );
+        posX -= tileSize_ * 2 * ( sceneWidth_ + 1 );
+        posY -= tileSize_ * ( sceneWidth_ - 1 );
     }
     return to_render;
 }
@@ -46,11 +41,11 @@ QList<QGraphicsPixmapItem*> FightMap::getFloor()
 QList<QGraphicsPixmapItem *> FightMap::getEntities()
 {
     QList<QGraphicsPixmapItem*> to_render;
-    foreach (QList<Tile*> row, map_) {
-        foreach (Tile *tile, row) {
+    foreach (auto rows, map_) {
+        foreach (Tile *tile, rows) {
             if ( tile->entity_ ) {
-                 tile->entity_->sprite_->setPos(50, 50);
-                 to_render.append(tile->entity_->sprite_);
+                 tile->entity_->getSprite()->setPos(tile->topSprite_->x() + tileSize_/2, tile->topSprite_->y() - tileSize_);
+                 to_render.append(tile->entity_->getSprite());
             }
         }
     }
@@ -59,9 +54,14 @@ QList<QGraphicsPixmapItem *> FightMap::getEntities()
 
 void FightMap::setEntities(QList<QPair<Entity*, QPoint>> list)
 {
-    for (int i=0; i<list.size(); i++) {
-        map_[list[i].second.rx()][list[i].second.ry()]->entity_=list[i].first;
+    foreach (auto element, list) {
+        map_[element.second.y()][element.second.x()]->entity_=element.first;
     }
+}
+
+void FightMap::setFloor()
+{
+
 }
 
 
--- Ophio/fightmap.h
+++ Ophio/fightmap.h
@@ -1,32 +1,28 @@
 #ifndef FIGHTMAP_H
 #define FIGHTMAP_H
 
-#include <QGraphicsScene>
-#include <tilesetsplitter.h>
-
+#include "tilesetsplitter.h"
 #include "tile.h"
 
 class FightMap
 {
+
 public:
-    FightMap(int scene_size, int tile_size, TilesetSplitter *tileSplit);
+    FightMap(int sceneWidth, int sceneHeigh, TilesetSplitter *tileSplit);
 
 public:
     QList<QGraphicsPixmapItem*> getFightMap();
     QList<QGraphicsPixmapItem*> getFloor();
-    QList<QGraphicsPixmapItem *> getEntities();
+    QList<QGraphicsPixmapItem*> getEntities();
     void setEntities(QList<QPair<Entity*, QPoint>> list);
-
-//    TODO
     void setFloor();
-
-public:
-    int tile_px=16;
 
 private:
     QList<QList<Tile*>> map_;
-    int scene_size_;
-    int tile_size_;
+    int sceneWidth_;
+    int sceneHeigh_;
+    int tileSize_=16;
+    int tileZoom_=1;
 };
 
 #endif // FIGHTMAP_H
