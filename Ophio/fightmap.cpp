ffca7f70829aa5b63e11d5c3cb2608673c548e74
francoisg <francois.guern@trialog.com>
2021-04-18 14:09:44 +0200

    sizeTile_ not hardcoded / review entity position / check if entity spawn in map

--- Ophio/fightmap.cpp
+++ Ophio/fightmap.cpp
@@ -4,11 +4,14 @@ FightMap::FightMap(int sceneWidth, int sceneHeigh, TilesetSplitter *tileSplit):
     sceneWidth_(sceneWidth),
     sceneHeigh_(sceneHeigh)
 {
-    for (int i=0; i<sceneHeigh_; i++) {
+    for (int y=0; y<sceneHeigh_; y++) {
         map_.append(QList<Tile*>());
-        for (int j=0; j<sceneWidth_; j++) {
-            map_[i].append(new Tile(tileSplit));
+        for (int x=0; x<sceneWidth_; x++) {
+            map_[y].append(new Tile(tileSplit));
         }
+    }
+    if ( not(map_.empty()) ){
+        tileSize_=map_[0][0]->topSprite_->boundingRect().height();
     }
 }
 
@@ -25,10 +28,10 @@ QList<QGraphicsPixmapItem*> FightMap::getFloor()
     QList<QGraphicsPixmapItem*> to_render;
     int posX = 0;
     int posY = 0;
-    for (int i=0; i< sceneHeigh_; i++) {
-        for (int j=0; j<sceneWidth_; j++) {
-            map_[i][j]->topSprite_->setPos(posX, posY);
-            to_render.append(map_[i][j]->topSprite_);
+    for (int y=0; y< sceneHeigh_; y++) {
+        for (int x=0; x<sceneWidth_; x++) {
+            map_[y][x]->topSprite_->setPos(posX, posY);
+            to_render.append(map_[y][x]->topSprite_);
             posX += tileSize_*2;
             posY += tileSize_;
         }
@@ -44,8 +47,12 @@ QList<QGraphicsPixmapItem *> FightMap::getEntities()
     foreach (auto rows, map_) {
         foreach (Tile *tile, rows) {
             if ( tile->entity_ ) {
-                 tile->entity_->getSprite()->setPos(tile->topSprite_->x() + tileSize_/2, tile->topSprite_->y() - tileSize_);
-                 to_render.append(tile->entity_->getSprite());
+//                int w = tile->entity_->getSprite()->boundingRect().width();
+                int h = tile->entity_->getSprite()->boundingRect().height();
+                int posX = tile->topSprite_->x() + tileSize_/2;
+                int posY = tile->topSprite_->y() + tileSize_*0.75 - h;
+                tile->entity_->getSprite()->setPos(posX, posY);
+                to_render.append(tile->entity_->getSprite());
             }
         }
     }
@@ -55,7 +62,12 @@ QList<QGraphicsPixmapItem *> FightMap::getEntities()
 void FightMap::setEntities(QList<QPair<Entity*, QPoint>> list)
 {
     foreach (auto element, list) {
-        map_[element.second.y()][element.second.x()]->entity_=element.first;
+        if (element.second.y() <= sceneHeigh_ && element.second.x() <= sceneWidth_ ) {
+            map_[element.second.y()][element.second.x()]->entity_=element.first;
+        }
+        else {
+            qDebug() << "WARMING:" << "Entity ->" << element.first->getName() << "tried to spawn out of the map";
+        }
     }
 }
 
